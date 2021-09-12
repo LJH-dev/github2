209bc565aba8a1ceb49d6e2c92f018ba67ae2343
ljh <ljh@git.com>
2021-09-12 11:18:04 +0800

    hot-fix second commit

--- src/ThirtyNinth.java
+++ src/ThirtyNinth.java
@@ -6,6 +6,7 @@ public class ThirtyNinth {
     public static void main(String[] args) throws IOException {
         two();
         one();
+        three();
     }
 
     public static void one() throws IOException {
